1322b6a510fdfb42caff356f783709b0524e0fc7
thanhson5 <154438080+thanhson5@users.noreply.github.com>
2024-11-05 03:22:27 +0700

    Add files via upload

---
+++ Baitap1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Tongchanle1Den100 {
+
+    public static void main(String[] args) {
+    int sumOdd = 0, sumEven = 0;
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                sumEven += i;
+            } else {
+                sumOdd += i;
+            }
+        }
+        System.out.println("Tong so chan: " + sumEven);
+        System.out.println("Tong so le: " + sumOdd);
+    }
+} 
